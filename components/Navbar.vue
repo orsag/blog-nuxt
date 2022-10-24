52efadc16afbf9a1864405be3a4c8c7abf2aed54
Martin Orsag <martin.orsag108@gmail.com>
2022-10-24 13:45:55 +0200

    Optimized version.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,13 +5,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <NuxtLink class="nav-link active" aria-current="page" href="/">Home</NuxtLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/blogs">Listing</a>
+          <NuxtLink class="nav-link" href="/blogs">Listing</NuxtLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
+          <NuxtLink class="nav-link" href="/contact">Contact</NuxtLink>
         </li>
       </ul>
     </div>
--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,3 @@
-const FLOWERS1 = 'https://images.unsplash.com/photo-1568010967378-b92ea68220c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3774&q=80';
-
-const FLOWERS2= 'https://images.unsplash.com/photo-1558594278-e45f60d17fe0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3869&q=80';
-
 const PARAGRAPH1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et ipsum suscipit, dignissim purus ac, tristique magna. Nam sit amet tincidunt massa, ut tempus diam. Mauris aliquet magna eu lorem varius, ut tincidunt augue aliquam. Sed fringilla tortor vitae hendrerit semper. Pellentesque eget pulvinar magna, quis tempus urna. Quisque felis augue, elementum eget mollis nec, feugiat et quam. Curabitur finibus tristique velit, eu facilisis diam pulvinar vitae. Proin ac sodales eros, sit amet venenatis elit. Maecenas egestas ipsum in velit ullamcorper rhoncus. Cras suscipit, ligula et rhoncus sodales, libero sapien feugiat lectus, at suscipit erat justo eget sapien. Maecenas imperdiet nisl in pellentesque mattis. Proin justo massa, tincidunt sed turpis nec, varius mattis urna. Cras tincidunt hendrerit sem, id consequat ante ultricies at. Mauris at lectus laoreet, dapibus nibh vitae, mollis lorem. Praesent interdum at dolor at accumsan. Fusce et ipsum in urna ultricies accumsan.'
 
 const PARAGRAPH2 = 'In hac habitasse platea dictumst. Ut ut interdum magna. Donec suscipit augue at neque gravida, quis molestie justo tempor. Mauris non leo dolor. Phasellus purus lectus, dapibus non aliquet quis, viverra tincidunt elit. Maecenas accumsan nisi id tincidunt finibus. Maecenas eu nisi cursus, sodales tellus vitae, mattis dui. Duis pharetra odio sit amet neque lobortis vestibulum. Aliquam dignissim in metus vel euismod. Etiam nec elit non risus accumsan placerat at eu nisi. Cras consectetur porttitor metus, id lobortis libero consequat sit amet. Suspendisse nec ultrices tellus. In hac habitasse platea dictumst. Aenean mollis egestas quam sed ornare.'
@@ -13,8 +9,6 @@ const PARAGRAPH4 = 'In sapien tortor, tempus a risus id, gravida dignissim leo. 
 const PARAGRAPH5 = 'Aenean cursus, purus eu auctor rhoncus, lacus erat consequat enim, eu feugiat elit ipsum eu tellus. Sed enim mi, pellentesque quis ante id, pretium auctor justo. Proin varius, sem nec consequat blandit, ex metus consectetur libero, ut molestie est orci in erat. Vivamus lacinia dui et nulla aliquet sodales. Etiam et orci enim. Ut et dignissim odio. Morbi faucibus luctus purus. Cras dolor neque, lobortis vel efficitur et, mollis eget velit. Donec ultrices auctor diam in tincidunt. Nam efficitur, erat ut eleifend porta, eros augue sollicitudin risus, viverra vulputate nibh augue eu magna. In bibendum dui eget eleifend ornare. Suspendisse convallis eros lorem. Vestibulum lobortis semper condimentum. Nunc vitae tempor nibh, at convallis nibh. Nam eget sollicitudin purus.'
 
 export {
-  FLOWERS1,
-  FLOWERS2,
   PARAGRAPH1,
   PARAGRAPH2,
   PARAGRAPH3,
